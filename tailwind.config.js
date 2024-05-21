771c45386eabb255b8c1d5eb4d5475dc5dbfca95
MoNakouzi <mo.nakouzi03@gmail.com>
2024-05-21 01:50:19 -0400

    adding brown-300 to tailwind config for bg gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        brown: {
+          300: "#A4550A",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
