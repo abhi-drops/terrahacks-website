c53182a6a145586f841c97b07e9ce955f6cea038
monakouzi <mo.nakouzi03@gmail.com>
2024-06-01 04:57:18 -0400

    making images webp for optimization

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,12 +19,12 @@ module.exports = {
         },
       },
       backgroundImage: {
-        "full-image": "url('/assets/backgrounds/full-bg.png')",
-        "laptop-image": "url('/assets/backgrounds/laptop-bg.png')",
-        "phone-image": "url('/assets/backgrounds/phone-bg.png')",
+        "full-image": "url('/assets/backgrounds/full-bg.webp')",
+        "laptop-image": "url('/assets/backgrounds/laptop-bg.webp')",
+        "phone-image": "url('/assets/backgrounds/phone-bg.webp')",
 
-        "hero-section": "url('/assets/backgrounds/intro.png')",
-        "hero-section-mobile": "url('/assets/backgrounds/intro-mobile.png')",
+        "hero-section": "url('/assets/backgrounds/intro.webp')",
+        "hero-section-mobile": "url('/assets/backgrounds/intro-mobile.webp')",
       },
     },
   },
