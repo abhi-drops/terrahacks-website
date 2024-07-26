822184121f4bdf5149f6b59e00680aeca281571b
Prachi Patel <prachipatel0415@gmail.com>
2024-07-26 00:36:28 -0400

    Added new sponsors and updated deadline date

--- src/components/sections/sponsors/sponsors.jsx
+++ src/components/sections/sponsors/sponsors.jsx
@@ -27,7 +27,7 @@ export default function Sponsors() {
             }
         };
 
-        
+
         if (typeof window !== 'undefined') {
             window.addEventListener('keydown', handleKeyDown);
         }
@@ -83,15 +83,15 @@ export default function Sponsors() {
                 </a>
                 <div className="main max-h-[65%] md:max-h-[75%] grid grid-cols-5 grid-rows-5 gap-2 md:gap-4 lg:gap-6 xl:gap-10 2xl:gap-12 items-center justify-center w-full pt-6 lg:pt-10 2xl:pt-14">
                     {sponsors.map((sponsor, index) => {
-                        
-                    if (sponsor.isEmpty){
-                        return (
-                            <div
-                                key={index}
-                                className="col-span-1 h-full w-full rounded-md shadow-md bg-transparent"
-                            ></div>  );
-                    }else 
-                        {
+                        if (sponsor.isEmpty) {
+                            return (
+                                <div
+                                    key={index}
+                                    className="col-span-1 h-full w-full rounded-md shadow-md bg-transparent"
+                                />
+                            );
+                        }
+                        else {
                             return (
                                 <button
                                     aria-label={`Link to ${sponsor.fullName}`}
@@ -104,12 +104,12 @@ export default function Sponsors() {
                                         alt={sponsor.fullName}
                                         width={500}
                                         height={500}
-                                        className={`object-contain w-full h-full pointer-events-none ${sponsor.shortName === 'CS TMU' && 'py-2 rounded-md'}`}
+                                        className={`object-contain w-full h-full pointer-events-none ${sponsor.shortName === 'CS TMU' && 'py-2 rounded-md'} ${sponsor.shortName === 'Armed Forces' && 'px-2 py-1 rounded-md'} ${sponsor.shortName === 'FoS' && 'px-2 rounded-md'}`}
                                     />
                                 </button>
                             );
-                        
-                        }})}
+                        }
+                    })}
                 </div>
             </div>
             <div className="absolute w-[25%] 2xl:w-[17%] left-[77%] md:left-[72%] 2xl:left-[70%] top-[25.8%] md:top-[25.5%] lg:top-[27.6%] 2xl:top-[27.6%]">
